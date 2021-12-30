b9ee5a1be2e3f5cb87a930ad81472dfc643acbe7
DHOSTEEN <32811716+DHOSTEEN@users.noreply.github.com>
2021-12-30 19:22:12 +0000

    day 10 complete

--- AdventofCode2021/src/Day10/SyntaxAutoFill.java
+++ AdventofCode2021/src/Day10/SyntaxAutoFill.java
@@ -5,6 +5,7 @@
 package Day10;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  *
@@ -12,11 +13,52 @@ import java.util.ArrayList;
  */
 public class SyntaxAutoFill extends SyntaxLogic{
 
+private String[] line;
+private ArrayList<String> additions;
+
 public SyntaxAutoFill(){super();}
 
+    public ArrayList<String> getAdditions(String[] line){
 
-    public void recursiveAutoFill(String target, int depth, int pos){
+        this.line = line;
+        additions = new ArrayList<>();
 
+        for(int i =0; i<line.length; i++){
+
+            if(parenthesis_pair.get(line[i]) != null){
+                recursiveAutoFill(parenthesis_pair.get(line[i]), 0, i+1);
+            }
+        }
+        Collections.reverse(additions);
+        return additions;
+    }
+
+    private void recursiveAutoFill(String target, int depth, int pos){
+
+        //System.out.println("TARGET: " + target);
+        //System.out.println("POS: " + pos);
+        //System.out.println("DEPTH: " + depth);
+        if(pos>=line.length){
+            additions.add(target);
+            return;
+        }
+//if(target.equals(line[pos]) && depth == 0 && !is_done)
+        if(target.equals(line[pos]) && depth == 0){
+            //System.out.println("IN HERE");
+            //discard () pair
+        }
+        else if(is_openParenthesis(line[pos])){
+           // System.out.println("In branch");
+            //count_open++;
+            recursiveAutoFill(target, depth+1, pos+1);
+        }
+        else if(depth ==0){
+            System.out.println("I SHOULD NEVER PROC");
+   
+        }
+        else{
+            recursiveAutoFill(target, depth-1, pos+1);
+        }
     }
 }
    
--- AdventofCode2021/src/Day10/Syntax_Scoring_Main.java
+++ AdventofCode2021/src/Day10/Syntax_Scoring_Main.java
@@ -16,21 +16,27 @@ import java.util.Arrays;
 public class Syntax_Scoring_Main {
 
 private static SyntaxReader obj;
-private static String file_location = "E:\\Netbeans\\AdventofCode2021\\AdventInputs\\AdventInput10_testdata.txt";
+private static String file_location = "E:\\Netbeans\\AdventofCode2021\\AdventInputs\\AdventInput10_data.txt";
 private static String testy = "{([(<{}[<>[]}>{[]{[(<()>";
 private static String testy2 ="([<(((((())";
+
 public static void main(String[] args){
 
     obj = new SyntaxReader(file_location);
     SyntaxLogic logic = new SyntaxLogic();
     SyntaxAutoFill auto_fill = new SyntaxAutoFill();
-    String[] test = testy2.split("");
+    /*String[] test = testy2.split("");
 
     System.out.println(Arrays.toString(test));
     System.out.println(logic.recursive(test));
-    //System.out.println(Arrays.toString(obj.getLine()));
-    /*ArrayList<String[]> all_lines = obj.getAllLines();
+    System.out.println("Additions: " + auto_fill.getAdditions(test).toString());
+ 
+    //System.out.println(Arrays.toString(obj.getLine()));*/
+    ArrayList<String[]> all_lines = obj.getAllLines();
+    ArrayList<String> addons = new ArrayList<>();
+    ArrayList<Long> all_products = new ArrayList<>();
     int count = 0;
+    long products =0;
     for(String[] str_ar : all_lines){
 
         String result = logic.recursive(str_ar);
@@ -48,14 +54,44 @@ public static void main(String[] args){
                 count += 25137;
                 break;
             case "incomplete line":
-                System.out.println(Arrays.toString(str_ar) +  " add: " + auto_fill.findPair(str_ar).toString());
-
+                addons = auto_fill.getAdditions(str_ar);
+                products = calc(addons);
+                all_products.add(products);
         }      
         //System.out.println(result);
         //System.out.println(Arrays.toString(str_ar));
 
     }
-    System.out.println("COUNT: " + count);*/
+    all_products.sort(null);
+       // System.out.println(all_products.toString());
+    System.out.println("COUNT: " + count);
+    System.out.println("SIZE: " + all_products.size());
+    System.out.println("MIDDLE: " + all_products.get(all_products.size()/2));
 }
+
+    public static long calc(ArrayList<String> str_arr){
+        long product = 0;
+        for(int i =0; i<str_arr.size(); i++){
+        switch(str_arr.get(i)){
+            case ")":
+                product =  product * 5;
+                product += 1;
+                break;
+            case "]":
+              product =  product * 5;
+                product += 2;
+                break;
+            case "}":
+              product =  product * 5;
+                product += 3;
+                break;
+            case ">":
+              product =  product * 5;
+                product += 4;
+                break;
+        }
+        }
+        return product;
+    }
     
 }
